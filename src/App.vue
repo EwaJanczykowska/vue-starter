c92015dbb0a2c50462f9a041ff5fa5c610da66a4
Ewa Janczykowska <janczykowska@student.agh.edu.pl>
2020-04-25 10:56:57 +0200

    login form

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <h1>Twój e-mail to {{ email }}</h1>
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
 
-    <div v-if="email.length < 10">Ale masz krótki adres!</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-
-    <input type="email" v-model="email"/>
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <div v-if="authenticatedUserName">
+      <h3>Witaj {{ authenticatedUserName }}!</h3>
+      <a href="#" @click="logMeOut()">Wyloguj</a>
+    </div>
+    <div v-else>
+      Zaloguj się e-mailem
+      <input type="email" v-model="email"/>
+      <button @click="logMeIn()">Wchodzę</button>
+    </div>
 
   </div>
 </template>
@@ -16,13 +19,18 @@
 export default {
   data() {
     return {
-      email: ''
+      email: '',
+      authenticatedUserName: ''
     };
   },
 
   methods: {
-    alertMyEmail() {
-      alert(this.email);
+    logMeIn() {
+      this.authenticatedUserName = this.email;
+      this.email = '';
+    },
+    logMeOut() {
+      this.authenticatedUserName = '';
     }
   }
 }
