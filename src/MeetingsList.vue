f9fb3dd678900bdf841b0d6d20a9ca74d40e36a8
Ewa Janczykowska <janczykowska@student.agh.edu.pl>
2020-05-06 21:27:36 +0200

    improved meeting list table

--- src/MeetingsList.vue
+++ src/MeetingsList.vue
@@ -1,28 +1,36 @@
 <template>
-    <table v-if="meetings.length > 0">
-        <thead>
-        <tr>
-            <th>Nazwa spotkania</th>
-            <th>Opis</th>
-            <th>Uczestnicy</th>
-            <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="meeting in meetings" :key="meeting.name">
-            <td>{{ meeting.name }}</td>
-            <td>{{ meeting.description }}</td>
-            <td></td>
-            <td style="text-align: right">
-                <button id="enrollButton" class="button button-outline">Zapisz się</button>
-                <button>Usuń puste spotkanie</button>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-    <div v-else>
-        Brak zaplanowanych spotkań
+    <div>
+        <div v-if="meetings.length > 0">
+            <h3>Zaplanowane zajęcia ({{meetings.length}})</h3>
+
+            <table>
+                <thead>
+                <tr>
+                    <th>Nazwa spotkania</th>
+                    <th>Opis</th>
+                    <th>Uczestnicy</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="meeting in meetings" :key="meeting.name">
+                    <td>{{ meeting.name }}</td>
+                    <td>{{ meeting.description }}</td>
+                    <td></td>
+                    <td style="text-align: right">
+                        <button id="enrollButton" class="button button-outline">Zapisz się</button>
+                        <button>Usuń puste spotkanie</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div v-else>
+            Brak zaplanowanych spotkań
+        </div>
     </div>
+
+
 </template>
 
 <script>
